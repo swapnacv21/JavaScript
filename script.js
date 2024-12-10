4d401509d5e208a55fdab8573af4e323237e735d
swapnacv21 <swapnavenu116@gmail.com>
2024-12-10 12:47:26 +0530

    JavaScript

--- script.js
+++ script.js
@@ -171,6 +171,67 @@
 // o/p : true
 
 
+// .................................................
+
+function sample(){
+    // method : 1
+    // ....................
+
+    // let result=document.getElementById("name")
+    // console.log(result.value);
+    // document.getElementById("display").innerHTML=result.value
+
+    // method : 2
+    // ...................
+
+    // let result=document.getElementById("name").value
+    // console.log(result);
+    // let h2=document.getElementById("display")
+    // h2.innerHTML=result
+    
+    // number value
+    // ....................
+
+    // let no1=parseInt(document.getElementById("no1").value) 
+    // let no2=parseInt(document.getElementById("no2").value)
+    // let h2=document.getElementById("display")
+    // h2.innerHTML=no1+no2
+}
+
+function add(){
+
+    let no1=parseInt(document.getElementById("no1").value) 
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1+no2
+}
+
+function sub(){
+    let no1=parseInt(document.getElementById("no1").value) 
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1-no2
+}
+
+function mul(){
+    let no1=parseInt(document.getElementById("no1").value) 
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1*no2
+}
+function div(){
+    let no1=parseInt(document.getElementById("no1").value) 
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1/no2
+}
+function mod(){
+    let no1=parseInt(document.getElementById("no1").value) 
+    let no2=parseInt(document.getElementById("no2").value)
+    let h2=document.getElementById("display")
+    h2.innerHTML=no1%no2
+}
+
 
 
 
