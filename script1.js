9c311bb699ea9bc3442cf2140e1b1ace78cd4cdc
swapnacv21 <swapnavenu116@gmail.com>
2024-12-12 13:41:01 +0530

    methods of string,array,task

---
+++ script1.js
@@ -0,0 +1,145 @@
+// let a='welcome'
+// console.log(a)
+// console.log(a.length);  // output: 7
+// console.log(a[0]); // output: w
+
+// methods of string
+// ....................
+
+// console.log(a.charAt(1));
+// output : e
+
+// console.log(a.concat(' ','To all'));
+// output: welcome To all
+
+// console.log(a.endsWith('e'));
+// output : true
+// console.log(a.endsWith('e'));
+// output: false
+
+// console.log(a.startsWith('w'));
+// output: true
+
+// console.log(a.toUpperCase());
+// output: WELCOME
+
+// console.log(a.toLowerCase());
+// output : welcome
+
+// console.log(a.trim());
+// output  : welcome
+
+// console.log(a.trimEnd());
+
+// console.log(a.trimStart());
+// ................................................................................................
+
+
+
+// methods of array
+// ...........................
+
+// let a=['apple','orange','grape']
+// console.log(a.length);
+// output: 3
+
+// console.log(a[0]);
+// output: apple
+
+// a[0]='APPLE'
+// console.log(a);
+// output : ["APPLE","orange","grape"]
+
+// a.push(('kiwi'))
+// console.log(a);
+// output : ["APPLE","orange","grape","kiwi"]
+
+// a.pop()
+// console.log(a);
+// output : ["APPLE","orange"]
+
+// a.shift()
+// console.log(a);
+// output : ["orange","grape"]
+
+// a.unshift("banana")
+// console.log(a);
+// output : ["banana","APPLE","orange","grape"]
+
+// ...........................................................................................
+
+// 1)for in
+// .........
+
+// let a=['apple','orange','grape']
+// for (i in a){
+//     console.log(i);
+// }
+// output : 0 1 2
+
+// let b='hello'
+// for (i in b){
+//     console.log(i);
+// }
+// output : 0 1 2 3 4
+
+// 2) for of 
+// ..........
+
+
+// let b='helo'
+// for (i of b){
+//     console.log(i);
+    
+// }
+
+// output:
+// h
+// e
+// l
+// 0
+
+// .............................................................................
+
+// functions in js
+// ..................
+
+// 1) user defined function
+
+// function fun1(a){
+//     console.log('fun1',a);    
+// }
+// fun1('helo')
+
+// output: fun1 helo
+
+// 2) nameless function
+
+// const data = function(a){
+//     console.log('data',a);
+// }
+// data('helo')
+
+// output: data helo
+
+// 3) arrow function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ task1.js
@@ -0,0 +1,25 @@
+let data = [{id:'1',name:'Liam',age:23},{id:'2',name:'Isa',age:20},{id:'3',name:'Jibin',age:21,},{id:'4',name:'Sandra',age:24,}]
+
+function Display(){
+    let tbody = document.querySelector("tbody")
+    data.forEach((user)=>{
+        let tr = document.createElement("tr")
+        
+        let id_td = document.createElement("td")
+        id_td.innerHTML = user.id
+        tr.appendChild(id_td)
+
+        let name_td=document.createElement("td")
+        name_td.innerHTML = user.name
+        tr.appendChild(name_td)
+
+        let age_td=document.createElement("td")
+        age_td.innerHTML = user.age
+        tr.appendChild(age_td)
+
+        tbody.appendChild(tr)
+
+
+    });
+}
+Display()
