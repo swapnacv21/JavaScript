5ba10ce09e85411a2e89c11fd52b8fce2078221f
swapnacv21 <swapnavenu116@gmail.com>
2024-12-18 13:24:57 +0530

    validations

--- sample.js
+++ sample.js
@@ -50,18 +50,85 @@
 // image timer
 // ......................
 
-let count = 10
-let data = setInterval(()=>{
-    document.getElementById("time").innerHTML=count
-    console.log(count);
-    count--
-    if (count<0){
-        clearInterval(data)
-        if(count<0){
-            document.getElementById("image").style.display='none'
-        }
+// let count = 10
+// let data = setInterval(()=>{
+//     document.getElementById("time").innerHTML=count
+//     console.log(count);
+//     count--
+//     if (count<0){
+//         clearInterval(data)
+//         if(count<0){
+//             document.getElementById("image").style.display='none'
+//         }
+//     }
+// },1000)
+
+
+// mobile  number validation
+// ..............................
+
+
+// document.getElementById("form").addEventListener("submit",function(event){
+//     event.preventDefault()
+//     let no = document.getElementById('no').value
+//     let a1 = document.getElementById("a1")
+//     if(/^[6-9]\d{9}/.test(no)){
+//         a1.innerHTML='Valid'
+//     }
+//     else{
+//         a1.innerHTML='Invalid'
+//     }
+// })
+
+// password validation
+// ............................................
+
+
+let pswd = document.getElementById("pswd")
+let cap = document.getElementById("cap")
+let len = document.getElementById("len")
+let sm = document.getElementById("sm")
+let no = document.getElementById("no")
+let spl = document.getElementById("spl")
+pswd.addEventListener('input',function(){
+    let password = pswd.value
+    // method 1:
+    // ...............................
+    // if(password.length>=8){
+    //     len.style.color='green'
+    // }
+    // else{
+    //     len.style.color='red'
+    // }
+    // ...............................
+
+    // method 2:
+    // .............................
+
+    len.style.color = password.length>=8 ? 'green' : 'red'
+    cap.style.color=/[A-Z]/.test(password) ? 'green' : 'red'
+    sm.style.color=/[a-z]/.test(password) ? 'green' : 'red'
+    no.style.color=/\d/.test(password) ? 'green' : 'red'
+    spl.style.color=/[!@#$%*]/.test(password) ? 'green' : 'red'
+})
+
+document.getElementById("form").addEventListener("submit",function(event){
+    event.preventDefault()
+    let password = document.getElementById("pswd").value
+
+    if (password.length>=8 && /[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password) && /[!@#$%*]/.test(password)){
+        alert('Valid')
     }
-},1000)
+    else{
+        alert("Invalid")
+    }
+
+})
+
+
+
+
+
 
 
 
